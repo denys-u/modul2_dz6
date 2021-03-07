815700dddfccac22717c73aacca913d047bc2971
denys-u <denys.udovik@nure.ua>
2021-03-07 18:37:27 +0200

    added gitignore file

---
+++ modul2_dz6/modul2_dz6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul2_dz6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
