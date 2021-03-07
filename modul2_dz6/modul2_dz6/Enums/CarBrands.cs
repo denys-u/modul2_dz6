59cdc9c56212c0f85ce4dbed6415b50969009100
denys-u <denys.udovik@nure.ua>
2021-03-07 20:01:42 +0200

    added Car Brands

---
+++ modul2_dz6/modul2_dz6/Enums/CarBrands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul2_dz6.Enums
+{
+    public enum CarBrands
+    {
+        Volkswagen,
+        Peugeot,
+        Renault,
+        Toyota,
+        Honda,
+        Suzuki
+    }
+}
