e00f5930bb8d55aa1974d6a6718c321fcbcfd37d
denys-u <denys.udovik@nure.ua>
2021-03-09 19:32:13 +0200

    describe parlexstentions

--- modul2_dz6/modul2_dz6/Helpers/Extentions/Parkextentions.cs
+++ modul2_dz6/modul2_dz6/Helpers/Extentions/Parkextentions.cs
@@ -1,4 +1,5 @@
 ﻿using Modul2_dz6.models;
+using Modul2_dz6.models.Enums;
 using Modul2_dz6.Result;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,29 @@ namespace modul2_dz6.Helpers.Extentions
 {
     public static class Parkextentions
     {
-        public static Car[] FindCars(this TaxiPark taxipark)
+        public static Car[] FindCars(this TaxiPark taxipark, ClassofCar carClass, double minPrice, double maxPrice)
         {
+            var temp = new Car[taxipark.Cars.Length];
 
+            for (var i=0; i < temp.Length; i++)
+            {
+                var item = taxipark.Cars[i];
+                if (item.ClassofCar == carClass && item.Price > minPrice && item.Price < maxPrice)
+                {
+                    temp[i] = taxipark.Cars[i];
+                }
+            }
+
+            var tempResult = ArrayHelper.CleanCapasity(temp);
+
+            var result = new Car[tempResult.Length];
+
+            for (var i = 0; i < result.Length; i++)
+            {
+                result[i]= tempResult[i] as Car;
+            }
+
+            return result;
         }
     }
 }
